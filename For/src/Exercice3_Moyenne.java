5da064664b35432088c40a1bdbc1ac1e9c5dfe66
Faisal <maarij_faisal@hotmail.com>
2025-10-21 20:54:40 +0200

    Exercice 3 Calcule de la moyenne de N terminé

---
+++ For/src/Exercice3_Moyenne.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Exercice3_Moyenne {
+    public static void main(String[] args) {
+        /*
+        Réécrire un programme qui calcule la moyenne d’une série de n nombres entrés par l’utilisateur.
+        La valeur de n est fournie en préambule par l’utilisateur (cf. boucle while).
+        */
+        Scanner scan1 = new Scanner(System.in);
+        Scanner scan2 = new Scanner(System.in);
+        int moyennne = 0;
+        System.out.println("Combien des nombres voulez vous entrez pour connaitre leur moyenne? ");
+        int userMax = scan1.nextInt();
+        int userNombres = 0;
+        for (int i = 1; i <= userMax; i++) {
+            System.out.println("Veuillez entrez mnt les nombre " + i + " :");
+            userNombres = userNombres + scan2.nextInt();
+            moyennne = userNombres / userMax;
+        }
+        System.out.println("Voila la moyenne des " + userMax + " nombres entrée = " + moyennne);
+    }
+}
